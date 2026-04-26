a57fbf0edc98c998037e4848ccab38e55d1d48b8
ZhulidovAl <fine.zhulidov@gmail.com>
2026-04-26 18:51:03 +0500

    fix:

--- src/main/java/com/example/wedsite/controller/AdminController.java
+++ src/main/java/com/example/wedsite/controller/AdminController.java
@@ -1,18 +1,21 @@
 package com.example.wedsite.controller;
 
 import com.example.wedsite.dto.UserDto;
-import com.example.wedsite.entity.User;
-import com.example.wedsite.repository.UserRepository;
 import com.example.wedsite.service.AdminService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.context.annotation.Scope;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PageableDefault;
-import org.springframework.stereotype.Controller;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.access.annotation.Secured;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
